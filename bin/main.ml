bc637438c9799b16a6f319089aa28b1f7d9b04fa
Johannes Maier <johannes.maier@mailbox.org>
2024-02-24 09:09:34 +0100

    Minimize the problem
    Calling luaL_register with an empty list of registrations fails
    already.

--- bin/main.ml
+++ bin/main.ml
@@ -2,16 +2,10 @@ module LuaL = Lua_api.LuaL
 module Lua = Lua_api.Lua
 module Sdl = Tsdl.Sdl
 
-let lua_foo ls =
-  let n = Lua.tointeger ls (-1) in
-  Lua.pushinteger ls (n * 17);
-  1
-;;
-
 let () =
   let ls = LuaL.newstate () in
   LuaL.openlibs ls;
-  LuaL.register ls (Some "lib") [ "foo", lua_foo ];
+  LuaL.register ls (Some "lib") [];
   if LuaL.dofile ls "test.lua" then print_endline "ok" else print_endline "not ok";
   Sdl.init Sdl.Init.(video + events) |> ignore;
   match Sdl.create_window_and_renderer ~w:500 ~h:300 Sdl.Window.mouse_focus with
