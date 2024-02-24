bbc1b961dd9936ac265ca2d5cfeeb76c0208933d
Johannes Maier <johannes.maier@mailbox.org>
2024-02-24 09:09:13 +0100

    Add ability to close the window

--- bin/main.ml
+++ bin/main.ml
@@ -16,7 +16,16 @@ let () =
   Sdl.init Sdl.Init.(video + events) |> ignore;
   match Sdl.create_window_and_renderer ~w:500 ~h:300 Sdl.Window.mouse_focus with
   | Ok (_w, r) ->
-    while true do
+    let quit = ref false in
+    (* event handling is only needed to be able to more nicely close the window
+       again *)
+    let e = Sdl.Event.create () in
+    while not !quit do
+      if Sdl.poll_event (Some e)
+      then (
+        match Sdl.Event.(enum (get e typ)) with
+        | `Quit -> quit := true
+        | _ -> ());
       Sdl.set_render_draw_color r 20 20 20 200 |> ignore;
       Sdl.render_clear r |> ignore;
       Sdl.render_present r
